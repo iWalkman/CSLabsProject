c05968b1c84048c7d00c0cf5a263ea6ce3b78e1a
kfomin <kosty.fomin98@yandex.ru>
2018-12-03 07:34:39 +0300

    Initial Commit

---
+++ CSLab1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CSLab1
+//
+//  Created by Константин Фомин on 03/12/2018.
+//  Copyright © 2018 Константин. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
